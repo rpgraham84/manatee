b66e604896e6951293063b0e223982f6cf59b68e
rpgraham84 <rpgraham84@gmail.com>
2014-04-19 23:32:30 -0400

    Didn't fix tab spacing last time. Got it now.

--- manatee.py
+++ manatee.py
@@ -13,19 +13,19 @@ main_soup = BeautifulSoup(request.text)
 available = [baseurl+a.get("href") for a in main_soup.find("ul").findAll("a")]
 
 for url in available:
-	new_soup = BeautifulSoup(requests.get(url).text)
-	poster = {
-		"available": (new_soup.find(\
+  new_soup = BeautifulSoup(requests.get(url).text)
+  poster = {
+    "available": (new_soup.find(\
       id="availability").text.find("Sold Out") < 0),
-		"url": url,
-		"price": new_soup.find(class_="price").text,
-		"title": new_soup.find(class_="page_headers").text,
-	}
-	try:
-		poster["editionof"] = int(edition_regex.findall(\
+    "url": url,
+    "price": new_soup.find(class_="price").text,
+    "title": new_soup.find(class_="page_headers").text,
+  }
+  try:
+    poster["editionof"] = int(edition_regex.findall(\
       new_soup.find(class_="item").text)[0])
-	except:
-		pass
-	posters.append(poster)
-	
+  except:
+    pass
+  posters.append(poster)
+  
 print json.dumps(posters, indent=2)
