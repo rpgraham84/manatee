a909fab066e7e420f15611823badfb07d2ea2fa2
rpgraham84 <rpgraham84@gmail.com>
2014-04-19 23:29:49 -0400

    Fixed tab spacing. Lines cut to 80 chars.

--- manatee.py
+++ manatee.py
@@ -15,13 +15,15 @@ available = [baseurl+a.get("href") for a in main_soup.find("ul").findAll("a")]
 for url in available:
 	new_soup = BeautifulSoup(requests.get(url).text)
 	poster = {
-		"available": (new_soup.find(id="availability").text.find("Sold Out") < 0),
+		"available": (new_soup.find(\
+      id="availability").text.find("Sold Out") < 0),
 		"url": url,
 		"price": new_soup.find(class_="price").text,
 		"title": new_soup.find(class_="page_headers").text,
 	}
 	try:
-		poster["editionof"] = int(edition_regex.findall(new_soup.find(class_="item").text)[0])
+		poster["editionof"] = int(edition_regex.findall(\
+      new_soup.find(class_="item").text)[0])
 	except:
 		pass
 	posters.append(poster)
