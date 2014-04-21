f8f33f37f29ffc09451850b0de2f2b667464694f
rpgraham84 <rpgraham84@gmail.com>
2014-04-21 19:39:54 -0400

    Makes and requests in an infinite 30sec loop

--- manatee.py
+++ manatee.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
-
+from time import sleep
 from bs4 import BeautifulSoup
 import re
 import requests
 import json
 
-edition_regex = re.compile(r'Edition of (\d+)')
 posters = []
 baseurl = "http://www.mondotees.com/"
-request = requests.get(baseurl)
-main_soup = BeautifulSoup(request.text)
-available = [baseurl+a.get("href") for a in main_soup.find("ul").findAll("a")]
+edition_regex = re.compile(r'Edition of (\d+)')
 
-for url in available:
-  new_soup = BeautifulSoup(requests.get(url).text)
-  poster = {
-    "available": (new_soup.find(\
-      id="availability").text.find("Sold Out") < 0),
-    "url": url,
-    "price": new_soup.find(class_="price").text,
-    "title": new_soup.find(class_="page_headers").text,
-  }
-  try:
-    poster["editionof"] = int(edition_regex.findall(\
-      new_soup.find(class_="item").text)[0])
-  except:
-    pass
-  posters.append(poster)
+while True:
+  request = requests.get(baseurl)
+  main_soup = BeautifulSoup(request.text)
+  available =\
+    [baseurl+a.get("href") for a in main_soup.find("ul").findAll("a")]
+
+  if posters != []:
+    oldposters = posters
+
+  posters = []
+
+  for url in available:
+    new_soup = BeautifulSoup(requests.get(url).text)
+    poster = {
+      "available": new_soup.find(id="availability").text.find("Sold Out") < 0,
+      "url": url,
+      "price": new_soup.find(class_="price").text,
+      "title": new_soup.find(class_="page_headers").text,
+    }
+
+    try:
+      poster["editionof"] =\
+        int(edition_regex.findall(new_soup.find(class_="item").text)[0])
+    except:
+      pass
+
+    posters.append(poster)
   
-print json.dumps(posters, indent=2)
+  print json.dumps(posters, indent=2)
+  sleep(30)
